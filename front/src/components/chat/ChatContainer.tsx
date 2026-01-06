733b9295c13df688f5ae8d744fabb92f5e432a0c
David Dominguez <daviddominguez@Davids-MacBook-Air.local>
2026-01-06 17:36:39 -0500

    check

--- front/src/components/chat/ChatContainer.tsx
+++ front/src/components/chat/ChatContainer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import { useChatStore } from "@/stores/chat-store";
 
 import { MessageList } from "./MessageList";
@@ -9,6 +10,15 @@ export function ChatContainer() {
   const messages = useChatStore((state) => state.messages);
   const isLoading = useChatStore((state) => state.isLoading);
   const sendMessage = useChatStore((state) => state.sendMessage);
+
+  // Log all messages to console every 10 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("[Messages]", messages);
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, [messages]);
 
   return (
     <div className="flex h-full flex-col">
